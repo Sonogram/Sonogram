1ab7585f3feb94a8f765c278ef8ebbcd395306f3
TMiguelT <ttmigueltt@gmail.com>
2017-02-27 00:43:17 +1100

    Restructured into large switch case. Fixed grammar

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
+// Imports
 const path = require('path')
 const pug = require('pug')
 const WebSocket = require('ws');
+
 // Load configuration from local config files
 const conf = require('rc')('sonogram', {
     port: 8080,
     grammar: 'general'
 });
+
+// Directory containing all the grammar files
 const grammarBase = path.join(__dirname, 'grammar', 'langs')
+// The subscribed editors
+const editors = []
+// The server
+const wss = new WebSocket.Server({port: conf.port});
 
-let recognizer, editor;
-
-function setRecognizer(ws) {
-    console.log('Recognizer connected!')
-    recognizer = ws
-
-    // Process messages from the recognizer
-    ws.on('message', msgStr => {
-        const msg = JSON.parse(msgStr)
+wss.on('connection', ws => {
+    ws.on('message', message => {
+        const msg = JSON.parse(message)
         switch (msg.action) {
+
+            // An editor wants to subscribe to commands
+            case 'register':
+                console.log('Editor connected!')
+                editors.push(ws)
+                break
 
             // The recognizer wants a specific grammar XML file
             // Must include a 'lang' and 'vars' parameter
@@ -28,6 +36,7 @@ function setRecognizer(ws) {
                         action: 'grammar',
                         grammar
                     }))
+                    console.log(`"${msg.lang}" grammar sent`)
                 }
                 else {
                     ws.send(JSON.stringify({
@@ -40,55 +49,14 @@ function setRecognizer(ws) {
             // The recognizer has identified some speech
             // Must include a 'speech' parameter
             case 'recognition':
-                if (!editor)
+                if (editors.length == 0)
                     console.warn('Recognizer input received, but no editor is connected!')
-                else
-                    editor.send(msg.speech)
+                else {
+                    for (let editor of editors)
+                        editor.send(msg.speech)
+                    console.log(`"${msg.speech}" recognized and sent to ${editors.length} editor(s)`)
+                }
                 break
         }
     })
-}
-
-function setEditor(ws) {
-    console.log('Editor connected!')
-    editor = ws
-}
-
-const wss = new WebSocket.Server({port: conf.port});
-/**
- * Wait for connection. Once we get one, wait for an identification message. If identification is successful, remove
- * the identification listener, and add new listeners for their role
- */
-wss.on('connection', ws => {
-    ws.on('message', awaitIdentification);
-
-    function awaitIdentification(message) {
-        try {
-            const msg = JSON.parse(message)
-            if (msg.action == 'identify') {
-                if (msg.role == 'recognizer') {
-                    setRecognizer(ws)
-                    ws.removeListener('message', awaitIdentification)
-                }
-                else if (msg.role == 'editor') {
-                    setEditor(ws)
-                    ws.removeListener('message', awaitIdentification)
-                }
-                else
-                    throw new Error
-            }
-        }
-        catch (ex) {
-            ws.send(JSON.stringify({
-                type: 'error',
-                message: `Invalid identification message. The first message sent to this server by a JSON object of the
-                 form: 
-                 {
-                    "action": "identify",
-                    "type": "editor" | "recognizer"
-                 }`
-            }))
-        }
-    }
-
-});
+})
